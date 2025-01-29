3b75d1d7fd63b92feaaf761bdc8b90fe179089e7
Rekar-J <civil.eng2019s@gmail.com>
2025-01-29 15:43:11 +0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -2,9 +2,7 @@
 
 import streamlit as st
 from streamlit_option_menu import option_menu
-from style import apply_sidebar_styles
 
-# Sidebar Rendering Function
 def render_sidebar():
     with st.sidebar:
         selected_tab = option_menu(
@@ -13,6 +11,5 @@ def render_sidebar():
             icons=["house", "tools", "calendar", "file-check", "gear", "people"],
             menu_icon="menu-button",
             default_index=0,
-            styles=apply_sidebar_styles()
         )
     return selected_tab
