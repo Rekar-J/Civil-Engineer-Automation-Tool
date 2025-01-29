a46b785b6e8b5baa5377871d41f02f1aadd74ebf
Rekar-J <civil.eng2019s@gmail.com>
2025-01-29 15:28:25 +0300

    Create sidebar.py

---
+++ sidebar.py
@@ -0,0 +1,17 @@
+### sidebar.py ###
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+from style import apply_styles
+
+def render_sidebar():
+    with st.sidebar:
+        selected_tab = option_menu(
+            "Main Menu",
+            ["Home", "Design and Analysis", "Project Management", "Compliance and Reporting", "Tools and Utilities", "Collaboration and Documentation"],
+            icons=["house", "tools", "calendar", "file-check", "gear", "people"],
+            menu_icon="menu-button",
+            default_index=0,
+            styles=apply_styles()
+        )
+    return selected_tab
