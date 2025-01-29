1215c4602c61d93d180c9016ee32fbac1a0f67d5
Rekar-J <civil.eng2019s@gmail.com>
2025-01-29 15:34:00 +0300

    Update style.py

--- style.py
+++ style.py
@@ -1,9 +1,10 @@
-### style.py ###
+# style.py
 
-def apply_styles():
+# Centralized styling for the application
+def apply_sidebar_styles():
     return {
-        "container": {"padding": "5px"},
+        "container": {"padding": "5px", "background-color": "#f0f2f6"},
         "icon": {"color": "orange", "font-size": "25px"},
-        "nav-link": {"font-size": "20px", "text-align": "left", "margin": "0px"},
+        "nav-link": {"font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#FF5733"},
     }
