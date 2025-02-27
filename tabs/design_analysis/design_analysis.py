82b22001e57b8387265eba4539e9e42bfe2d93b0
Rekar-J <civil.eng2019s@gmail.com>
2025-02-27 17:12:13 +0300

    Update design_analysis.py

--- tabs/design_analysis/design_analysis.py
+++ tabs/design_analysis/design_analysis.py
@@ -17,13 +17,23 @@ def run():
     ])
 
     with tabs[0]:  
+        # Force reload of structural_analysis.py
+        import importlib
+        import tabs.structural_analysis
+        importlib.reload(tabs.structural_analysis)
         structural_analysis()
 
     with tabs[1]:  
+        import tabs.geotechnical_analysis
+        importlib.reload(tabs.geotechnical_analysis)
         geotechnical_analysis()
 
     with tabs[2]:  
+        import tabs.hydraulic_analysis
+        importlib.reload(tabs.hydraulic_analysis)
         hydraulic_analysis()
 
     with tabs[3]:  
+        import tabs.tests
+        importlib.reload(tabs.tests)
         tests()
