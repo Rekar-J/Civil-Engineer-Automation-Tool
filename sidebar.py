b476122f5c214458aae163d3247e24a44e0e1a9c
Rekar-J <civil.eng2019s@gmail.com>
2025-04-20 22:55:39 +0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -3,11 +3,11 @@ from streamlit_option_menu import option_menu
 
 def render_sidebar():
     with st.sidebar:
-        selected_tab = option_menu(
+        return option_menu(
             "Main Menu",
-            ["Home", "Design and Analysis", "Project Management", "Compliance and Reporting", "Tools and Utilities", "Collaboration and Documentation"],
-            icons=["house", "tools", "calendar", "file-check", "gear", "people"],
+            ["Home", "Design and Analysis", "Project Management",
+             "Compliance and Reporting","Tools and Utilities","Collaboration and Documentation"],
+            icons=["house","tools","calendar","file-check","gear","people"],
             menu_icon="menu-button",
             default_index=0,
         )
-    return selected_tab
