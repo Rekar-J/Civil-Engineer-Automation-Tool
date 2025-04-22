be05533d53084bac311e615c13559826f83fbfb6
Rekar-J <civil.eng2019s@gmail.com>
2025-04-22 16:41:36 +0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,13 +1,32 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from style import apply_sidebar_styles
 
 def render_sidebar():
+    """
+    Render the main sidebar with custom styles.
+    """
     with st.sidebar:
-        return option_menu(
+        selected_tab = option_menu(
             "Main Menu",
-            ["Home", "Design and Analysis", "Project Management",
-             "Compliance and Reporting","Tools and Utilities","Collaboration and Documentation"],
-            icons=["house","tools","calendar","file-check","gear","people"],
+            [
+                "Home",
+                "Design and Analysis",
+                "Project Management",
+                "Compliance and Reporting",
+                "Tools and Utilities",
+                "Collaboration and Documentation"
+            ],
+            icons=[
+                "house",
+                "tools",
+                "calendar",
+                "file-check",
+                "gear",
+                "people"
+            ],
             menu_icon="menu-button",
             default_index=0,
+            styles=apply_sidebar_styles()
         )
+    return selected_tab
